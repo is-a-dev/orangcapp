c5f9627a20906bfab5ad90e8d75d4d5c98dccc92
MaskDuck <maskduck@proton.me>
2024-04-15 22:04:38 +0700

    add is-a.dev docs link to nohelp

--- _orangcbot/extensions/tags.py
+++ _orangcbot/extensions/tags.py
@@ -7,6 +7,7 @@ Hey you! This is not a help channel!\n
 We have a help channel for a reason, <#1155589227728339074>.\n
 Please use that as a help channel. We need you to open a thread, even if it is a tiny problem.\n
 So... **Enjoy troll answers past this message and take them with a pinch of salt. The action of following any help in this channel past this message is at your own risk. You've been warned.**\n
+Please check the [is-a.dev docs](https://www.is-a.dev/docs/)\n
 Have fun!"""
 class Tags(commands.Cog):
     def __init__(self, bot):
