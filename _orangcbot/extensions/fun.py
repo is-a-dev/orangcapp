c726f541839283d82a8d4facda63d0934693a2ed
MaskDuck <maskduck@proton.me>
2024-04-18 18:29:26 +0700

    add urbandict

--- _orangcbot/extensions/fun.py
+++ _orangcbot/extensions/fun.py
@@ -1,6 +1,7 @@
 from nextcord.ext import commands
 from psl_dns import PSL
 import nextcord
+import aiohttp
 
 _psl = PSL()
 from typing import Optional
@@ -42,6 +43,29 @@ class Fun(commands.Cog):
             await ctx.send("Yes, we are.")
         else:
             await ctx.send("No, we aren't.")
+    
+    @commands.command()
+    async def ubdict(self, ctx: commands.Context, *, word: str):
+        """contributed by vaibhav"""
+        params = {"term": word}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.urbandictionary.com/v0/define", params=params
+            ) as response:
+                data = await response.json()
+        if not data["list"]:
+            return await ctx.send("No results found.")
+        embed = nextcord.Embed(
+            title=data["list"][0]["word"],
+            description=data["list"][0]["definition"],
+            url=data["list"][0]["permalink"],
+            color=nextcord.Color.green(),
+        )
+        embed.set_footer(
+            text=f"👍 {data['list'][0]['thumbs_up']} | 👎 {data['list'][0]['thumbs_down']} | Powered by: Urban Dictionary"
+        )
+        await ctx.send(embed=embed)
+
 
 
 def setup(bot):
