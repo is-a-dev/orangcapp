21de1c370da8dc9f24b73dbd1d71e46d485634e7
MaskDuck <maskduck@proton.me>
2024-04-21 20:00:03 +0700

    Basic support of tags (#5)
    * Basic support of tags
    
    * h
    
    * Fix all bugs

--- _orangcbot/__main__.py
+++ _orangcbot/__main__.py
@@ -14,13 +14,15 @@ bot = commands.Bot(
     help_command=commands.DefaultHelpCommand(),
     case_insensitive=True,
 )
-@bot.event
-async def on_command_error(ctx, error):
-    k = await ctx.bot.create_dm(nextcord.Object(id=716134528409665586))
-    await k.send(traceback.format_exception(error))
+# @bot.event
+# async def on_command_error(ctx, error):
+#     k = await ctx.bot.create_dm(nextcord.Object(id=716134528409665586))
+#     await k.send(traceback.format_exception(error))
+#     print(traceback.format_exception(error))
   
 bot.load_extension("onami")
 bot.load_extension("extensions.fun")
-bot.load_extension("extensions.tags")
+if os.getenv("HASDB"):
+    bot.load_extension("extensions.tags_reworked")
 # bot.load_extension("extensions.forum")
 bot.run(environ["TOKEN"])
---
+++ _orangcbot/extensions/testing_functions.py
@@ -0,0 +1,20 @@
+from nextcord.ext import commands
+import nextcord
+
+class Testings(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self._bot: commands.Bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def hinder(self, ctx: commands.Context, cmd: str):
+        if cmd == "hinder": 
+            await ctx.send("I did not expect you to be such a fool")
+        else:
+            cmd = await self._bot.get_command(cmd)
+            cmd.enabled = not cmd.enabled
+            await ctx.send("Request satisfied, master.")
+
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(Testings(bot))
