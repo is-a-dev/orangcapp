afad0c19fa4882f00d40eec37a99c184ffcda42d
maskduck <maskduck@proton.me>
2024-05-30 12:55:46 +0700

    Add ruff config

---
+++ _orangcbot/extensions/emoji.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import nextcord
+from nextcord import Attachment, Interaction, SlashOption, slash_command
+from nextcord.ext import commands
+
+
+class Emoji(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self._bot: commands.Bot = bot
+
+    @slash_command()
+    async def add_emoji(
+        self,
+        interaction: Interaction,
+    ) -> None:
+        pass
+
+    @add_emoji.subcommand()
+    async def emojigg(
+        self,
+        interaction: Interaction,
+        emoji_id: str = SlashOption(description="The emoji.gg ID", required=True),
+    ) -> None: ...
