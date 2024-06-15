011560a7462bc1f8ab4d9ccf3a8f24891ec4c9dd
maskduck <maskduck@proton.me>
2024-06-15 12:23:38 +0700

    Make it more fair

--- _orangcbot/extensions/antihoist.py
+++ _orangcbot/extensions/antihoist.py
@@ -14,20 +14,17 @@ class Antihoist(commands.Cog):
 
     @commands.Cog.listener("on_member_join")
     async def check_nickname_on_join(self, member: nextcord.Member) -> None:
-        for x in member.display_name:
-            if not x in ascii_letters:
-                await member.edit(nick="kid", reason="attention seeking")
-                return
+        for x in ascii_letters:
+            if not member.display_name.startswith(x):
+                await member.edit(nick="kid", reason="imagine attention seeking")
 
     @commands.Cog.listener("on_member_edit")
     async def check_nickname_on_edit(
         self, _: nextcord.Member, after: nextcord.Member
     ) -> None:
-
-        for x in after.display_name:
-            if not x in ascii_letters:
-                await after.edit(nick="kid", reason="attention seeking")
-                return
+        for x in ascii_letters:
+            if not after.display_name.startswith(x):
+                await after.edit(nick="kid", reason="imagine attention seeking")
 
 
 def setup(bot: commands.Bot) -> None:
